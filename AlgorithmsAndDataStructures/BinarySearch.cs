c8f5f195299446ade4ae7ab30ba9b37aa0b807a1
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-06-03 20:31:21 +0530

    [Elavarasu] UC2-Binary Search of String

---
+++ AlgorithmsAndDataStructures/BinarySearch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsAndDataStructures
+{
+    internal class BinarySearch
+    {
+        public void Main()
+        {
+            string input = System.IO.File.ReadAllText(@"D:\BridgeLabz\Algorithms-and-DataStructures\AlgorithmsAndDataStructures\InputString.txt");
+            string[] arrayOfString = input.Split(',');
+            Array.Sort(arrayOfString);
+            Console.WriteLine("Sorted Array is:");
+            for (int i = 0; i < arrayOfString.Length; i++)
+            {
+                Console.Write(arrayOfString[i] + ",");
+            }
+            Console.WriteLine("\nEnter a word to search the file");
+            string userInput = Console.ReadLine();
+            int output = StringSearch(arrayOfString, 0, arrayOfString.Length - 1, userInput);
+
+            if (output == -1)
+            {
+                Console.WriteLine("Data not found in the file");
+            }
+            else
+            {
+                Console.WriteLine(userInput + " is at index " + output);
+            }
+        }
+        public int StringSearch(string[] array, int left, int right, string userInput)
+        {
+            while (left <= right)
+            {
+                int middle = left + (right - left) / 2;
+                int result = userInput.CompareTo(array[middle]);
+                if (result == 0)
+                {
+                    return middle;
+                }
+                if (result > 0)
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
--- AlgorithmsAndDataStructures/Program.cs
+++ AlgorithmsAndDataStructures/Program.cs
@@ -6,8 +6,30 @@ namespace AlgorithmsAndDataStructures
     {
         public static void Main(string[] args)
         {
-            PermutationOfString getMethod = new PermutationOfString();
-            getMethod.Main();
+            Console.WriteLine("Enter 1 for Permutation of String\n 2 for BinarySearch");
+            Console.WriteLine("Enter a Number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            switch (userInput)
+            {
+                case 1:
+                    {
+                        PermutationOfString getMethod = new PermutationOfString();
+                        getMethod.Main();
+                        break;
+                    }
+                case 2:
+                    {
+                        BinarySearch getMethod = new BinarySearch();
+                        getMethod.Main();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Enter a Valid Number");
+                        break;
+                    }
+            }
         }
     }
 }
