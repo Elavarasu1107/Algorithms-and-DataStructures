b4a9d427d8cfa6fe3ef5859450417c21c48be999
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-06-03 20:13:31 +0530

    [Elavarasu] UC1-Permutation of String

---
+++ AlgorithmsAndDataStructures/PermutationOfString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsAndDataStructures
+{
+    internal class PermutationOfString
+    {
+        public void Permutation(string inputString, string store)
+        {
+            if (inputString.Length == 0)
+            {
+                Console.Write(store + " ");
+                return;
+            }
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                char chars = inputString[i];
+                string subString1 = inputString.Substring(0, i);
+                string subString2 = inputString.Substring(i + 1);
+                string result = subString1 + subString2;
+                Permutation(result, store + chars);
+            }
+        }
+        public void Main()
+        {
+            String store = "";
+
+            Console.Write("Enter the string : ");
+            string inputString = Console.ReadLine();
+
+            Permutation(inputString, store);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AlgorithmsAndDataStructures
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PermutationOfString getMethod = new PermutationOfString();
+            getMethod.Main();
+        }
+    }
+}
